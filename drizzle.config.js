a6f469e8c91e62438611abd1029b11ec9ff9923a
Anup Kumar <anup1915683@gmail.com>
2025-08-30 17:16:50 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default defineConfig({
     dialect: "postgresql",
     schema: "./config/schema.js",
     dbCredentials: {
-        url: "postgresql://neondb_owner:npg_jk3sA8OWFqeE@ep-morning-cloud-ad9wmn2q-pooler.c-2.us-east-1.aws.neon.tech/AI%20lms?sslmode=require&channel_binding=require"
+        url: "postgresql://neondb_owner:npg_jk3sA8OWFqeE@ep-morning-cloud-ad9wmn2q-pooler.c-2.us-east-1.aws.neon.tech/lms%20db?sslmode=require&channel_binding=require"
     }
 });
